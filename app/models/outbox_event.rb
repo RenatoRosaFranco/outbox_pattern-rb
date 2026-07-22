552b2d54914578aac5e1b1b54fc15b4b0d84c017
Renato <renato_ny@live.com>
2026-07-21 20:03:54 -0600

    feat: improving payment and outbox_events model.

--- app/models/outbox_event.rb
+++ app/models/outbox_event.rb
@@ -1,2 +1,22 @@
+# frozen_string_literal: true
+
 class OutboxEvent < ApplicationRecord
+  # Enum
+  enum :status, {
+    pending: "pending",
+    published: "published"
+  }, default: :pending, validate: true
+
+  # TODO: extract this to a query interface
+  # Scopes
+  scope :pending_for_publication, -> {
+    pending.order(:created_at)
+  }
+
+  # TODO: extract this to a validation class (Validator)
+  # Validations
+  validates :aggregate_type, presence: true
+  validates :aggregate_id, presence: true
+  validates :event_type, presence: true
+  validates :payload, presence: true
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,2 +1,24 @@
+# frozen_string_literal: true
+
 class Payment < ApplicationRecord
+  # Enums
+  enum :status, {
+    pending: "pending",
+    approved: "approved",
+    rejected: "rejected"
+  }, default: :pending, validate: true
+
+  #  # TODO: extract this to a validation class (Validator)
+  # Validations
+  validates :account_id, presence: true
+  validates :amount_cents, numericality: {
+    only_integer: true,
+    greater_than: 0
+  }
+
+  validates :currency, presence: true,
+                       length: { is: 3 }
+
+  validates :idempotency_key, presence: true,
+                              uniqueness: true
 end
